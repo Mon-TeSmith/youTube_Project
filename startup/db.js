aeb541f057fcbaec92c19ea7778fd253c9849264
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-11-24 14:18:06 -0600

    created multiple folders and mongoose database

---
+++ startup/db.js
@@ -0,0 +1,17 @@
+const mongoose = require ("mongoose");
+const config = require ("config");
+const { func } = require("joi");
+
+function connectDB (){
+    mongoose.connect(
+        config.get("mogoURI"),
+        { useNewUrlParser: true , useUnifiedTopology: true})
+        .then(() => console.log("Connect to MongoDB..."))
+        .catch((err) => {
+            console.log(`Could not connect to MongoDB. ERROR: ${err}`);
+            process.exit(1);
+        });
+    
+}
+
+module.exports = connectDB;
